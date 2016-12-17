241836c9fda9ca6fe0ecb3166139c78266c392d2
John <johnconley507@hotmail.com>
2016-12-17 09:39:55 -0500

    setting interval for callback

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((request, response) => {
-  response.statusCode = 200;
+  response.statusCode = 200; 
   response.setHeader('Content-Type', 'text/plain');
+  setInterval (function () {
+ 	response.write(new Date() + "\n")
+}, 1000);
   response.end('Hello World\n');
+
 });
 
 server.listen(port, hostname, () => {
